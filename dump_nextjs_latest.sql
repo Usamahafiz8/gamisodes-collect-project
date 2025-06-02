145494372d4f8329783d50188ea6087f35443229
Muhammad Osama <usamahafiz8@gmail.com>
2025-06-02 20:38:33 +0000

    Add DB dumps and sesame backup

---
+++ dump_nextjs_latest.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:007dc2d3f96499a9bf1c61cd0217936e08cf24e0f6b0c7d9d87db46604b289c2
+size 163699668
---
+++ dump_prod.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:395fb436ea3d9215bf2bab80fb4e1dfe5521e53b3defe16a38aa853980e7807a
+size 620297
